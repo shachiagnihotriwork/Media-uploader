f83b2869e7af7d5328e3ea648359df6e7d5d80ba
Shachiagnihotriwork <Shachiagnihotriwork@gmail.com>
2025-12-05 11:30:48 +0000

    commiting upload code

---
+++ app.py
@@ -0,0 +1,78 @@
+from fastapi import FastAPI , HTTPException , File, UploadFile, Form, Depends
+from schema import PostCreate
+from db import Post
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
+from db import create_db_and_tables, get_async_session
+from sqlalchemy import select
+from images import imagekit
+from imagekitio.models.UploadFileRequestOptions import UploadFileRequestOptions
+import os
+import shutil
+import uuid
+import tempfile
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+app = FastAPI(lifespan=lifespan)
+
+
+@app.post("/upload")
+async def upload_file(
+    file : UploadFile = File(...),
+    caption : str = Form(""),
+    session : AsyncSession = Depends(get_async_session)
+):
+    temp_file_path = None
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(file.filename)[1]) as temp_file:
+            temp_file_path = temp_file.name
+            shutil.copyfileobj(file.file, temp_file)
+        upload_result = imagekit.upload_file(
+            file=open(temp_file_path, "rb"),
+            file_name=file.filename,
+            options=UploadFileRequestOptions(
+                use_unique_file_name=True,
+                tags=["Backend Upload"]
+            )
+        )
+        if upload_result.response_metadata.http_status_code == 200:
+            post = Post(
+                caption = caption,
+                url = upload_result.url,
+                file_type = "video" if file.content_type.startswith("video/") else "image",
+                file_name = upload_result.name
+                )
+            session.add(post)
+            await session.commit()
+            await session.refresh(post)
+            return post
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        if temp_file_path and os.path.exists(temp_file_path):
+            os.remove(temp_file_path)
+        file.file.close()
+
+@app.get("/feed")
+async def get_feed(
+    session : AsyncSession = Depends(get_async_session)
+):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+
+    post_data = []
+    for post in posts:
+        post_data.append({
+            "id": str(post.id),
+            "caption": str(post.caption),
+            "url": post.url,
+            "file_type": post.file_type,
+            "file_name": post.file_name,
+            "created_at": post.created_at.isoformat()
+
+        })
+    return {"posts" : post_data}
